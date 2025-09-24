8ba121eb8ecddaedb5c8510758bafaa860578396
Tandashi <18377875+Tandashi@users.noreply.github.com>
2025-09-24 20:42:04 +0200

    feat: initial commit

---
+++ generate-mods-json.ts
@@ -0,0 +1,17 @@
+import fs from 'node:fs';
+import { modDefinitionSchema } from './schema/schema';
+
+const modDefinitions = fs.readdirSync('./mods').filter(file => file.endsWith('.json'))
+  .flatMap(file => {
+    const filePath = `./mods/${file}`;
+    const modData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+    const validationResult = modDefinitionSchema.safeParse(modData);
+    if (!validationResult.success) {
+      console.error(`Validation failed for ${file}:`, validationResult.error);
+      return [];
+    }
+
+    return [validationResult.data];
+  });
+
+fs.writeFileSync('./mods.json', JSON.stringify(modDefinitions, null, 2));
---
+++ schema/schema.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+import { ZodSemver } from "zod-semver";
+
+export const modDefinitionSchema = z.object({
+  id: z.uuid(),
+  name: z.string().min(1),
+  description: z.string().optional(),
+  version: ZodSemver,
+  authors: z.array(z.string().min(1)),
+  download: z.object({
+    url: z.url(),
+    sha256: z.string().length(64),
+  }),
+  repository: z.url().optional(),
+  issues: z.url().optional(),
+  tags: z.array(z.enum(["texture-swap", "model"])),
+});
